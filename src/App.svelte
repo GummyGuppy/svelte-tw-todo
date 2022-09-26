bf7adb88171d7a3d64fb5426671bde793bee5a3d
GummyGuppy <alphastack.dev@gmail.com>
2022-09-26 09:30:35 -0500

    css changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
   
   import Button from './lib/Button.svelte'
+  import Footer from './lib/Footer.svelte'
+  
 
   let todos = []
 
@@ -17,11 +19,19 @@
 
 <div class="h-screen flex-col flex sm:flex-row">
 
-  <div class="bg-blue-100 h-1/2 sm:h-full sm:w-1/2">
+  <div class=" bg-blue-400 flex flex-col justify-end items-center h-1/2 sm:h-full sm:w-1/2">
 
+    <input class='w-72 mb-4'/>
   </div>
-  <div class="bg-yellow-300 h-1/2 sm:h-full sm:w-1/2">
 
+  <div class="flex flex-col h-1/2 sm:h-full sm:w-1/2 justify-between">
+
+    <div class="flex justify-center bg-red-200">
+      <Button title='Create' fn={create}/>
+    </div>
+    <div class='p-4 bg-red-200'>
+      <Footer/>
+    </div>
   </div>
 </div>
 
--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,21 +1,17 @@
 <script>
-import svelteLogo from './assets/svelte.svg'
+
+import Icon from '@iconify/svelte';
 
 </script>
 
-<div class="flex justify-center">
-    <a href="https://vitejs.dev" target="_blank">
-      <img
-        src="/vite.svg"
-        class="h-36 p-6 hover:drop-shadow-[0_0_2em_#646cffaa]"
-        alt="Vite Logo"
-      />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img
-        src={svelteLogo}
-        class="h-36 p-6 hover:drop-shadow-[0_0_2em_#ff3e00aa]"
-        alt="Svelte Logo"
-      />
-    </a>
+<div class="flex justify-center gap-2">
+    
+   
+      <Icon icon="logos:svelte-icon" height="80" class="hover:drop-shadow-[0_0_2em_#b8a3bb]"/>
+      <Icon icon="vscode-icons:file-type-tailwind" height="80" class="hover:drop-shadow-[0_0_2em_#15C2B8]"/>
+      <Icon icon="vscode-icons:file-type-vite" height="80" class="hover:drop-shadow-[0_0_2em_#8A2BE2]"/>
+
+    
+    
+    
 </div>
