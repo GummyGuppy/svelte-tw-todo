9fae9d8f25e8ce515358fcc7a8971f06a1712165
GummyGuppy <alphastack.dev@gmail.com>
2022-09-25 22:35:42 -0500

    changes after git issues

---
+++ src/lib/Button.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let title, fn
+  
+
+</script>
+
+<button
+  class="bg-[#f9f9f9] 
+        rounded-lg 
+        px-5 py-2 
+        font-medium 
+        border-2
+      hover:border-[#646cff]"
+      on:click={fn}
+    
+>
+{title}  
+</button>
---
+++ src/lib/Footer.svelte
@@ -0,0 +1,21 @@
+<script>
+import svelteLogo from './assets/svelte.svg'
+
+</script>
+
+<div class="flex justify-center">
+    <a href="https://vitejs.dev" target="_blank">
+      <img
+        src="/vite.svg"
+        class="h-36 p-6 hover:drop-shadow-[0_0_2em_#646cffaa]"
+        alt="Vite Logo"
+      />
+    </a>
+    <a href="https://svelte.dev" target="_blank">
+      <img
+        src={svelteLogo}
+        class="h-36 p-6 hover:drop-shadow-[0_0_2em_#ff3e00aa]"
+        alt="Svelte Logo"
+      />
+    </a>
+</div>
